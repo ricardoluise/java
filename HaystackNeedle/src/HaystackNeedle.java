323e03757e9afdee3850e6b9cfedadc2e257bd38
ricardo <ricardoluise@gmail.com>
2017-08-29 23:48:41 -0300

    final version

--- HaystackNeedle/src/HaystackNeedle.java
+++ HaystackNeedle/src/HaystackNeedle.java
@@ -1,6 +1,4 @@
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 
 public class HaystackNeedle {
 
@@ -8,12 +6,13 @@ public class HaystackNeedle {
 	private static Byte[] needleBytes;
 	private static Byte[] needleThreshold;
 	private static Byte[] haystackThreshold;
+	private static String foundAt ="Nothing found";
+	static boolean found=false;
 
 
 	public static void main(String[] args) {
 		
 		byte[] haystack = {'v','n','k','2','4','3','5','k','v','a','b','c','o','8','a','w','k','h','1','2','5','k','j','n','e','y','t','b','c','d','1','2','q','j','h','b','4','a','c','d','1','2','3','x','m','n','b','q','w','n','w','4','t'};
-		//byte[] haystack = {'a','b','c','d','1','2','3','4'};
 		byte[] needle = {'a','b','c','d','1','2','3','4'};
 	    int threshold = 3;
 
@@ -22,20 +21,31 @@ public class HaystackNeedle {
 	}
 	
 	
-	public static void search(byte[] haystack, byte[] needle, int threshold) {
+	public static void search(byte[] haystack, byte[] needle, int initialThreshold) {
 		
 		haystackBytes = toByte(haystack,haystack.length,0,haystack.length);
 		needleBytes = toByte(needle, needle.length,0,needle.length);
-		
-		for(int x=0; x<needleBytes.length; x++){
-			needleThreshold = toByte(needle, threshold,x,threshold+x);
-			for(int y=0; y<haystackBytes.length;y++){
-				haystackThreshold = toByte(haystack, threshold,y,threshold+y);
-				 if(Arrays.equals(needleThreshold, haystackThreshold)){
-					 		System.out.println("found");
-					 		
-				 }
+		int threshold= initialThreshold;
+		for(int y=0; y<(haystackBytes.length-threshold);y++){
+			for(int x=0; x<(needleBytes.length-threshold); x++){
+			
+				needleThreshold = toByte(needle, threshold,x,threshold);
+				haystackThreshold = toByte(haystack, threshold,y,threshold);
+				while(Arrays.equals(needleThreshold, haystackThreshold)){
+					found=true;
+					threshold++;					
+					needleThreshold = toByte(needle, threshold,x,threshold);
+					haystackThreshold = toByte(haystack, threshold,y,threshold);
+				 }		
+				
+				if(found){
+					foundAt = "sequence of length = "+(needleThreshold.length-1)+" found at haystack offset "+y+", needle offset "+x;
+					System.out.println(foundAt);
+					y=y+threshold;
+				}
+				found=false;
 			}
+			threshold= initialThreshold;	
 		}
 		
 	}
@@ -49,41 +59,6 @@ public class HaystackNeedle {
 	    }
 	    return newByte;
 	}
-
 	
 	
-	public static void search2(byte[] input, byte[] searchedFor, int threshold) {
-	    //convert byte[] to Byte[]
-	    Byte[] searchedForB = new Byte[searchedFor.length];
-	    for(int x = 0; x<searchedFor.length; x++){
-	        searchedForB[x] = searchedFor[x];
-	    }
-
-	    int idx = -1;
-
-	    //search:
-	    Deque<Byte> q = new ArrayDeque<Byte>(input.length);
-	    
-	    for(int i=0; i<input.length; i++){
-	        if(q.size() == searchedForB.length){
-	            //here I can check
-	            Byte[] cur = q.toArray(new Byte[]{});
-	            if(Arrays.equals(cur, searchedForB)){
-	                //found!
-	                idx = i - searchedForB.length;
-	                break;
-	            } else {
-	                //not found
-	                q.pop();
-	                q.addLast(input[i]);
-	            }
-	        } else {
-	            q.addLast(input[i]);
-	        }
-
-		 
-	    }
-	    System.out.println(idx);
-
-	}
 }
